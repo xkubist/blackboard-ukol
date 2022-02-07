7650f568bc8d7fe5f721eb5fc042c45ec29aa383
xkubist <zdenka@localhost.localdomain>
2022-02-07 16:59:19 +0100

    final changes

--- src/main/java/blackoard/interview/task/exception/ShortUrlNotReachableException.java
+++ src/main/java/blackoard/interview/task/exception/ShortUrlNotReachableException.java
@@ -1,6 +1,5 @@
 package blackoard.interview.task.exception;
 
-import blackoard.interview.task.urlShorting.ShortUrlIdTranslator;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
